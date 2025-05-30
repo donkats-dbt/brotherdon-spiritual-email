1f20082a5bb2dddd3befeceed8f1403607021926
donkats-dbt <78106637+donkats-dbt@users.noreply.github.com>
2025-05-30 05:02:19 -0500

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def get_connection():
-    return mysql.connector.connect(
+    return pymysql.connect(
         host=os.getenv("DB_HOST"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
